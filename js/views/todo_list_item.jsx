0ecdfef465c0dd70ed05c753aa8f1f7e65be92d6
Will Hastings <will@objectdotcreate.net>
2015-05-19 07:11:22 -0700

    Use PureRenderMixin in todo item view.

--- js/views/todo_list_item.jsx
+++ js/views/todo_list_item.jsx
@@ -1,6 +1,10 @@
 import React from 'react';
+import ReactAddons from 'react/addons';
+
+var PureRenderMixin = ReactAddons.addons.PureRenderMixin;
 
 export default React.createClass({
+  mixins: [PureRenderMixin],
   render() {
     var todo = this.props.todo;
     return <li className="todo-list__item">{todo.get('title')}</li>;
